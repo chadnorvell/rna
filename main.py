e8ce69d77135b8f9069ee5211d0ac2df47ac39ef
Kayce Basques <kayce@google.com>
2023-07-07 10:50:21 -0700

    Continue commit retrieval logic

--- main.py
+++ main.py
@@ -10,24 +10,71 @@ def get_config():
     return config
 
 
-def get_commits(owner, repo):
-    # Build the URL.
-    scheme = 'https'
-    netloc = 'api.github.com'
+def parse_scope(commit_message):
+    end = commit_message.index(':')
+    return commit_message[0:end]
+
+
+def get_commits(owner, repo, start, end):
+    page = 1
+    data = []
+    done = False
     path = f'/repos/{owner}/{repo}/commits'
-    params = ''
-    query = ''
-    fragment = ''
-    components = (scheme, netloc, path, params, query, fragment)
-    url = urllib.parse.urlunparse(components)
-    # Get the data.
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
-    response = requests.get(url, headers)
-    data = response.json()
-    print(data)
+    # Loop until we've got all the pages of data.
+    while not done:
+        # Build the GitHub API URL. The only thing that changes between iterations
+        # is the page number.
+        params = urllib.parse.urlencode({
+            'since': start,
+            'until': end,
+            'per_page': 100,
+            'page': page
+        })
+        url = urllib.parse.urlunparse(('https', 'api.github.com', path, '', params, ''))
+        # Get this page of data.
+        headers = {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+        response = requests.get(url, headers)
+        json = response.json()
+        if len(json) == 0:
+            done = True
+            continue
+        data += json
+        page += 1
+    # Weed out all the data we don't need.
+    commits = []
+    for commit in data:
+        url = commit['url']
+        sha = commit['sha']
+        message = commit['commit']['message']
+        date = commit['commit']['committer']['date']
+        commits.append({
+            'url': url,
+            'sha': sha,
+            'message': message,
+            'date': date
+        })
+    return commits
+
+
+def filter_commits(unfiltered_commits):
+    ok_commits = []
+    keywords = [
+        'Revert',
+        'Reland',
+        'roll'
+    ]
+    for commit in unfiltered_commits:
+        ok = True
+        scope = parse_scope(commit['message'])
+        for keyword in keywords:
+            if keyword in scope:
+                ok = False
+        if ok:
+            ok_commits.append(commit)
+    return ok_commits
 
 
 def main():
@@ -37,9 +84,8 @@ def main():
         repo = config['repo']
         start = config['start']
         end = config['end']
-        get_commits(owner, repo)
-        # commits = categorize_commits(unsorted_commits)
-        # print(json.dumps(commits, indent=4))
+        raw_commits = get_commits(owner, repo, start, end)
+        filtered_commits = filter_commits(raw_commits)
     except Exception as e:
         print('an unexpected error occurred')
         print(e)
