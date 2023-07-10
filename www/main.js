da4888135589c2d4327866dfb91a84834c26e07f
Kayce Basques <kayce@google.com>
2023-07-10 14:39:40 -0700

    Start commit retrieval logic

--- www/main.js
+++ www/main.js
@@ -1,1 +1,24 @@
-console.log('Hello, world!');
+function getCommits(owner, repo, start, end) {
+  let page = 1;
+  let done = false;
+  while (!done) {
+    let url = new URL(`https://api.github.com/repos/${owner}/${repo}/commits`);
+    const params = {
+      'since': start,
+      'until': end,
+      'per_page': 100,
+      'page': page
+    };
+    for (let key in params) {
+      url.searchParams.append(key, params[key]);
+    }
+    done = true;
+    console.log(url);
+  }
+}
+
+function main() {
+  getCommits();
+}
+
+main();
